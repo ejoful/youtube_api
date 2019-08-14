facc903846c6ccb9c1c2756289957795ab5cbf64
ejoful <ejoful@gmail.com>
2019-08-14 21:07:10 +0800

    first commit

---
+++ settings.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+MYSQL_HOST = '127.0.0.1'
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = 'root'
---
+++ ytb_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+class YoutubeApi:
+    'Youtube api'
+
+    def __init__(self, channelid):
+        self.channelid = channelid
+
+
+    def video_lists(self):
+        # TODO
+
+    def playlists(self):
+        # TODO
+
+    def playlists_items(self):
+        # TODO
+
+    def update_video(self):
+        # TODO
+
+    def update_playlist(self):
+        # TODO
+
+    def create_playlist(self):
+        # TODO
